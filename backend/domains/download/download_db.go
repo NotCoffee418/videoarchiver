7036fbfae609149e4b3ffff123bf43f424260592
Stijn Raeymaekers <9306304+NotCoffee418@users.noreply.github.com>
2025-08-30 19:01:46 +0200

    download_db: scanRows shared func

--- backend/domains/download/download_db.go
+++ backend/domains/download/download_db.go
@@ -21,21 +21,7 @@ func (d *DownloadDB) GetAllDownloads(limit int) ([]Download, error) {
 		return nil, err
 	}
 	defer rows.Close()
-
-	var downloads []Download
-	for rows.Next() {
-		var download Download
-		err := rows.Scan(
-			&download.ID, &download.PlaylistID, &download.Url,
-			&download.Status, &download.FormatDownloaded, &download.MD5, &download.OutputFilename,
-			&download.LastAttempt, &download.FailMessage, &download.AttemptCount,
-		)
-		if err != nil {
-			return nil, err
-		}
-		downloads = append(downloads, download)
-	}
-	return downloads, nil
+	return d.scanRows(rows)
 }
 
 func (d *DownloadDB) GetDownloadsForPlaylist(playlistId int) ([]Download, error) {
@@ -44,7 +30,10 @@ func (d *DownloadDB) GetDownloadsForPlaylist(playlistId int) ([]Download, error)
 		return nil, err
 	}
 	defer rows.Close()
+	return d.scanRows(rows)
+}
 
+func (d *DownloadDB) scanRows(rows *sql.Rows) ([]Download, error) {
 	var downloads []Download
 	for rows.Next() {
 		var download Download
