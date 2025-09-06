33b4bb38ba44295a1ad1a42fce31972f47d778c8
Stijn Raeymaekers <9306304+NotCoffee418@users.noreply.github.com>
2025-09-07 00:19:03 +0200

    wails autogen

--- frontend/wailsjs/go/models.ts
+++ frontend/wailsjs/go/models.ts
@@ -11,6 +11,8 @@ export namespace download {
 	    last_attempt: number;
 	    fail_message?: sql.NullString;
 	    attempt_count: number;
+	    save_directory?: sql.NullString;
+	    full_path?: sql.NullString;
 	
 	    static createFrom(source: any = {}) {
 	        return new Download(source);
@@ -28,6 +30,8 @@ export namespace download {
 	        this.last_attempt = source["last_attempt"];
 	        this.fail_message = this.convertValues(source["fail_message"], sql.NullString);
 	        this.attempt_count = source["attempt_count"];
+	        this.save_directory = this.convertValues(source["save_directory"], sql.NullString);
+	        this.full_path = this.convertValues(source["full_path"], sql.NullString);
 	    }
 	
 		convertValues(a: any, classs: any, asMap: boolean = false): any {
@@ -59,12 +63,12 @@ export namespace fileregistry {
 	    file_path: string;
 	    md5: string;
 	    registered_at: number;
-	    known_url?: sql.NullString;
-
+	    known_url: sql.NullString;
+	
 	    static createFrom(source: any = {}) {
 	        return new RegisteredFile(source);
 	    }
-
+	
 	    constructor(source: any = {}) {
 	        if ('string' === typeof source) source = JSON.parse(source);
 	        this.id = source["id"];
@@ -74,24 +78,24 @@ export namespace fileregistry {
 	        this.registered_at = source["registered_at"];
 	        this.known_url = this.convertValues(source["known_url"], sql.NullString);
 	    }
-
-	    convertValues(a: any, classs: any, asMap: boolean = false): any {
-	        if (!a) {
-	            return a;
-	        }
-	        if (a.slice && a.map) {
-	            return (a as any[]).map(elem => this.convertValues(elem, classs));
-	        } else if ("object" === typeof a) {
-	            if (asMap) {
-	                for (const key of Object.keys(a)) {
-	                    a[key] = new classs(a[key]);
-	                }
-	                return a;
-	            }
-	            return new classs(a);
-	        }
-	        return a;
-	    }
+	
+		convertValues(a: any, classs: any, asMap: boolean = false): any {
+		    if (!a) {
+		        return a;
+		    }
+		    if (a.slice && a.map) {
+		        return (a as any[]).map(elem => this.convertValues(elem, classs));
+		    } else if ("object" === typeof a) {
+		        if (asMap) {
+		            for (const key of Object.keys(a)) {
+		                a[key] = new classs(a[key]);
+		            }
+		            return a;
+		        }
+		        return new classs(a);
+		    }
+		    return a;
+		}
 	}
 
 }
